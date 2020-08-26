acc7bf1613dbf9ccf35ee8ccbf60ae71b699242a
shawnvdp <shawneevdploeg@live.nl>
2020-08-26 23:10:52 +0200

    added notes to rework the routes

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 const expressSanitizer = require("express-sanitizer"),
-  bodyParser = require("body-parser"),
   express = require("express"),
   app = express(),
-  puppeteer = require("puppeteer");
+  puppeteer = require("puppeteer"),
+  path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//express body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.use(expressSanitizer());
 
 
 let formData;
 
 app.get("/", (req, res) => {
-  res.redirect("/form");
+  res.redirect("form");
 });
+
+/*
+* TODO: NEW ROUTE LAYOUT -> PUT IN OWN ROUTE FOLDER {INVOICES/FORM}
+*
+//landing page overview
+app.get("/", (req, res) => {});
+//form to construct new PDF 
+app.get("/form", (req, res) => {});
+//retrieve all invoices, use to list all invoice files with clickable links to retrieve/display each one with the /invoices/:id route
+app.get("/invoices", (req, res) => {});
+//receive form data, construct PDF with request body data here -> redirect to /invoices/id after finished constructing PDF
+app.post("/invoices", (req, res) => {});
+//get specific PDF id (filename = invoice #?) -> safe PDF as file and then serve file with id of invoice number?
+app.get("/invoices/:id", (req, res) => {}); */
 
 app.get("/invoice", (req, res) => {
   res.render("invoices/invoice", { test: formData });
 });
 
 app.get("/form", (req, res) => {
-  res.render("/forms/form");
+  res.render("forms/form");
 });
 
+//send data using fetch in json format instead of form name attributes so we can expect json data here and handle it better
 app.post("/form", (req, res) => {
   formData = req.sanitize(req.body.test);
-  res.redirect("/export");
+  res.redirect("export");
 });
 
 app.get("/export", (req, res) => {
