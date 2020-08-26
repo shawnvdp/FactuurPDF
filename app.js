f66b0193b682ec363f8079905499849904b2c60a
shawnvdp <shawneevdploeg@live.nl>
2020-08-26 21:07:35 +0200

    setup dev environment with nodemon, express port checks for env var, added arrow functions

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
 const expressSanitizer = require("express-sanitizer"),
-bodyParser = require("body-parser"),
-express = require("express"),
-app = express(),
-puppeteer = require("puppeteer");
+  bodyParser = require("body-parser"),
+  express = require("express"),
+  app = express(),
+  puppeteer = require("puppeteer");
+
+const PORT = process.env.PORT || 3000;
 
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
+
 
 let formData;
 
-app.get("/", function(req, res){
-  res.render("forms/form");
+app.get("/", (req, res) => {
+  res.redirect("/form");
 });
 
-app.get("/invoice", function(req, res){
-  res.render("invoices/invoice", {test: formData});
+app.get("/invoice", (req, res) => {
+  res.render("invoices/invoice", { test: formData });
 });
 
+app.get("/form", (req, res) => {
+  res.render("/forms/form");
+});
 
-app.post("/form", function(req, res){
+app.post("/form", (req, res) => {
   formData = req.sanitize(req.body.test);
   res.redirect("/export");
 });
 
-app.get("/export", function(req, res){
+app.get("/export", (req, res) => {
   constructPDF()
-  .then(pdf => {
-    res.set({
-      "Content-Type": "application/pdf",
-      "Content-Length": pdf.length
+    .then(pdf => {
+      res.set({
+        "Content-Type": "application/pdf",
+        "Content-Length": pdf.length
+      });
+
+      res.send(pdf);
     });
-
-    res.send(pdf);
-  });
 });
 
-app.listen(3000, function(){
-  console.log("server listening on port 3000");
-});
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
 
-async function constructPDF(){
-  const browser = await puppeteer.launch({headless: true});
+async function constructPDF() {
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("http://localhost:3000/invoice", {waitUntil: 'networkidle0'});
-  const pdf = await page.pdf({format: 'A4', printBackground: true });
+  await page.goto("http://localhost:3000/invoice", { waitUntil: "networkidle0" });
+  const pdf = await page.pdf({ format: "A4", printBackground: true });
 
   await browser.close();
   return pdf;
